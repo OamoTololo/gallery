d947b9c80ebe78a4166901c222d7421871734bba
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-05-14 11:45:21 +0200

    Created the header panel, search bar (with reactive icon) panel with a button to add the user to the database

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="plugins/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/css/all.min.css">
+    <link rel="stylesheet" href="plugins/css/custom.css">
+
+</head>
+<body>
+
+<h1 class="bg-dark text-light text-center py-2">Gallery</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text bg-dark text-light"><i class="fas fa-search"></i></span>
+                </div>
+                <input type="text" class="form-control" placeholder="Search Image">
+            </div>
+        </div>
+        <div class="col-2">
+            <button class="btn btn-dark">Add Image</button>
+        </div>
+    </div>
+
+</div>
+
+
+
+<script src="plugins/js/jquery.js"></script>
+<script src="plugins/js/bootstrap.min.js"></script>
+<script src="plugins/js/all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+</body>
+</html>

5d861fa4f4f03f521094f9599349af833da24a95
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-05-14 15:31:30 +0200

    Added link tags in the table for the operations, created a view and edit modals.

---
+++ includes/imageDetailsModal.php
@@ -0,0 +1,20 @@
+<!-- Image Modal -->
+<div class="modal fade" id="viewModal" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Image Details</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="container" id="imageDetails">
+                    <h4>All image details.</h4>
+                </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Save</button>
+                    <button type="button" class="btn btn-outline-danger">Close</button>
+                </div>
+        </div>
+    </div>
+</div>
--- includes/imageTable.php
+++ includes/imageTable.php
@@ -1,3 +1,5 @@
+<?php include 'includes/imageDetailsModal.php'?>
+<?php include 'includes/editImageModal.php'?>
 <!-- Image Table -->
 <table class="table" id="imageTable">
     <thead class="table-dark">
@@ -16,9 +18,9 @@
             <td>XXXXXXX</td>
             <td>XXXXXXX</td>
             <td>
-                <span>Edit</span>
-                <span>Profile</span>
-                <span>Delete</span>
+                <a href="" class="mr-3 image" data-target="#viewModal" data-toggle="modal" title="View Image"><i class="fas fa-eye text-success"></i></a>
+                <a href=""class="mr-3 editImage" data-target="#editModal" data-toggle="modal" title="Edit Image"><i class="fas fa-edit text-info"></i></a>
+                <a href="" class="mr-3 deleteImage"><i class="fas fa-trash-alt text-danger" title="Delete Image"></i></a>
             </td>
         </tr>
     </tbody>
